c156a7b22831d1ef1c29e726d51c4fbb6f80a7d6
Sérgio Rodriguez <sperson@terra.com.br>
2018-09-26 14:47:48 +0100

    Add array.
    Add array.

--- MakingChange/MakingChange.java
+++ MakingChange/MakingChange.java
@@ -8,7 +8,8 @@ public class MakingChange {
 
 	public static void main(String[] args) {
 
-	double totalCost,totalPaid,change,note50,note20,note10,note5,coin2,coin1;
+	double totalCost,totalPaid,change,moneyAmmount;
+	int moneyValue[]={50,20,10,5,2,1};
 
 	Scanner in=new Scanner(System.in);
 
@@ -21,25 +22,13 @@ public class MakingChange {
 
 	System.out.println("Total change: " + Math.round(change));
 
-	note50=change/50;
-	change=Math.round(change)%50;
-	note20=change/20;
-	change=Math.round(change)%20;
-	note10=change/10;
-	change=Math.round(change)%10;
-	note5=change/5;
-	change=Math.round(change)%5;
-	coin2=change/2;
-	change=Math.round(change)%2;
-	coin1=change/1;
-	change=Math.round(change)%1;
+	for (int i=0;i<moneyValue.length;i++){
 
-	System.out.println("50 euro notes: " +Math.floor(note50));
-	System.out.println("20 euro notes: " +Math.floor(note20));
-	System.out.println("10 euro notes: " +Math.floor(note10));
-	System.out.println(" 5 euro notes: " +Math.floor(note5));
-	System.out.println(" 2 euro coins: " +Math.floor(coin2));
-	System.out.println(" 1 euro coins: " +Math.floor(coin1));
+		moneyAmmount=change/moneyValue[i];
+		change=Math.round(change)%moneyValue[i];
+
+		System.out.println(moneyValue[i]+" euro ammount: "+Math.floor(moneyAmmount));
+	}
 
 	}
 }
